80503a6b75ca0dd9fd03467cb38120c69c55ac29
Cedric Kaufmann <60580186+ced0ps@users.noreply.github.com>
2020-10-15 10:52:21 +0200

    Upstream sync 10.1.0 (#46)
    * [upstream-sync] Version 9.4.8
    
    * Update for new upstream chart location, required by deprecation of
    helm/stable
    
    * Keep version variable for automation. Add maintainers and new sources
    
    * Modify image reference to allow for different image registries.
    Add logic for Values.Installation
    
    * Remove to mirror upstream
    
    * Remove trunc
    
    * Update helm/prometheus-operator-app/values.yaml
    
    * Add truncation of chartref
    
    * Update helm/prometheus-operator-app/templates/prometheus-operator/deployment.yaml
    
    Co-authored-by: Quentin Bisson <quentin@giantswarm.io>
    
    * Update helm/prometheus-operator-app/templates/prometheus-operator/crds.yaml
    
    * Sync modifications to crds from upstream
    
    * Keep version variable and refere to correct resources
    
    * Delete files according to changes in upstream
    
    * Re-apply repository referencing and apply upstream changes
    
    * Apply upstream changes
    
    * Update CHANGELOG.md
    
    Co-authored-by: Quentin Bisson <quentin@giantswarm.io>

--- hacks/update-chart.sh
+++ hacks/update-chart.sh
@@ -7,9 +7,10 @@ set -o pipefail
 LAST_SYNC_COMMIT=$(git log -n1 --grep '^\[upstream-sync\]' --pretty='format:%h' helm/)
 TMPDIR=$(mktemp -d)
 
-UPSTREAM_ZIP_URL=${UPSTREAM_ZIP_URL:-"https://github.com/helm/charts/archive/master.zip"}
-UPSTREAM_CHART_DIR=${UPSTREAM_CHART_DIR:-"charts-master/stable"}
-UPSTREAM_CHART_NAME=${UPSTREAM_CHART_NAME:-"prometheus-operator"}
+UPSTREAM_ZIP_URL=${UPSTREAM_ZIP_URL:-"https://github.com/prometheus-community/helm-charts/archive/main.zip"}
+UPSTREAM_CHART_DIR=${UPSTREAM_CHART_DIR:-"helm-charts-main/charts"}
+UPSTREAM_CHART_NAME=${UPSTREAM_CHART_NAME:-"kube-prometheus-stack"}
+LOCAL_CHART_NAME=${LOCAL_CHART_NAME:-"prometheus-operator-app"}
 
 KEEP_TMPDIR=${KEEP_TMPDIR:-''}
 
@@ -17,15 +18,15 @@ clean_on_exit() {
   [[ -z "${KEEP_TMPDIR}" ]] && rm -rf ${TMPDIR}
 }
 
-curl -sL -o ${TMPDIR}/master.zip ${UPSTREAM_ZIP_URL}
-unzip -d ${TMPDIR}/ ${TMPDIR}/master.zip ${UPSTREAM_CHART_DIR}/${UPSTREAM_CHART_NAME}/*
+curl -sL -o ${TMPDIR}/main.zip ${UPSTREAM_ZIP_URL}
+unzip -d ${TMPDIR}/ ${TMPDIR}/main.zip ${UPSTREAM_CHART_DIR}/${UPSTREAM_CHART_NAME}/*
 
 
 UPSTREAM_VERSION=$(cat ${TMPDIR}/${UPSTREAM_CHART_DIR}/${UPSTREAM_CHART_NAME}/Chart.yaml | awk '/^version:/ { print $2 }')
 
-cp -a ${TMPDIR}/${UPSTREAM_CHART_DIR}/${UPSTREAM_CHART_NAME}/* helm/${UPSTREAM_CHART_NAME}-app/
+cp -a ${TMPDIR}/${UPSTREAM_CHART_DIR}/${UPSTREAM_CHART_NAME}/* helm/${LOCAL_CHART_NAME}/
 
-git add helm/${UPSTREAM_CHART_NAME}-app/
+git add helm/${LOCAL_CHART_NAME}/
 git commit -m "[upstream-sync] Version ${UPSTREAM_VERSION}"
 
 trap clean_on_exit EXIT
