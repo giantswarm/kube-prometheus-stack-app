bbe543ebf101c8f36c9b25849f8c4408a94daf7f
QuentinBisson <quentin@giantswarm.io>
2023-10-17 14:52:58 +0200

    Rename to kube-prometheus-stack
    Signed-off-by: QuentinBisson <quentin@giantswarm.io>

--- script/update-docs.sh
+++ script/update-docs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# This script generate the README.md in helm/prometheus-operator-app helm chart
+# This script generate the README.md in helm/kube-prometheus-stack helm chart
 
 tag=$(git describe --tags --abbrev=0)
 
-sed -i 's/\[\[ .Version \]\]/'"$tag"'/' ./helm/prometheus-operator-app/Chart.yaml
+sed -i 's/\[\[ .Version \]\]/'"$tag"'/' ./helm/kube-prometheus-stack/Chart.yaml
 
 docker run --rm --volume "$(pwd):/helm-docs" -u $(id -u) jnorwood/helm-docs:latest
 
-git checkout ./helm/prometheus-operator-app/Chart.yaml
+git checkout ./helm/kube-prometheus-stack/Chart.yaml
